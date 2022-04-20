84ecf23c550a2dc37910b7855f065d653482d11c
Lucas Ewing <lucas.ewing2000@gmail.com>
2022-04-20 16:36:42 -0700

    fin

--- sql.normalized_batch/02.sql
+++ sql.normalized_batch/02.sql
@@ -1,18 +1,15 @@
-/*
- * Calculates the hashtags that are commonly used with the hashtag #coronavirus
- */
-SELECT
+SELECT 
     tag,
-    count(*) as count
+    count(*) AS count
 FROM (
     SELECT DISTINCT
-        id_tweets,
-        t2.tag
-    FROM tweet_tags t1
-    JOIN tweet_tags t2 USING (id_tweets)
-    WHERE t1.tag='#coronavirus'
-      AND t2.tag LIKE '#%'
-) t
-GROUP BY (1)
-ORDER BY count DESC,tag
+        data->>'id' AS id_tweets,
+        '#' || (jsonb_array_elements(data->'entities'->'hashtags'
+            || COALESCE(data->'extended_tweet'->'entities'->'hashtags', '[]'))->>'text') AS tag -- Self join
+    FROM tweets_jsonb
+    WHERE data->'entities'->'hashtags' @@ '$[*].text == "coronavirus"' 
+       OR data->'extended_tweet'->'entities'->'hashtags' @@ '$[*].text == "coronavirus"' -- Get tags that mention coronavirus
+) t1
+GROUP BY tag
+ORDER BY count DESC, tag
 LIMIT 1000;
--- sql.normalized_batch/04.sql
+++ sql.normalized_batch/04.sql
@@ -1,9 +1,5 @@
-/*
- * Calculates the hashtags that are commonly used for English tweets containing the word "coronavirus"
- */
-SELECT
-    count(*)
-FROM tweets
-WHERE to_tsvector('english',text)@@to_tsquery('english','coronavirus')
-  AND lang='en'
-;
+SELECT count(DISTINCT data->>'id')
+FROM tweets_jsonb
+WHERE to_tsvector('english', COALESCE(data->'extended_tweet'->>'full_text', data->>'text'))
+      @@ to_tsquery('english', 'coronavirus')
+  AND data ->>'lang' = 'en';  
